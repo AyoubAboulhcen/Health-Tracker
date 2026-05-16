a45394e72e4a24415bd73690ccd8e2cc5980f4ea
Ayoub Aboulhcen <aboulhcenayoub@gmail.com>
2026-05-16 14:53:21 +0100

    Clean: stop tracking database and update gitignore

--- src/select_chart.py
+++ src/select_chart.py
@@ -1,22 +1,19 @@
-from charts import main_chart , calories_chart
+
+from charts import main_chart, calories_chart
 
 def chart():
     print("what are the charts you want to see ?")
     print(" type 1 for weight progression over time with the ideal healh range ")
-    print(" type 2 for calories chart ")
-
+    print(" type 2 for calories taken vs calories needed ")
+    
 def slt_chart():
-
-    value = input()
+    
     for i in range(3):
-         value = input()
-         if value not in ["1","2"]:
-            print("please enter a valid number")
-            value = input()
-         elif value == "1" :
-            main_chart ()
-            
-         elif value == "2" :
-            calories_chart ()
-         
-slt_chart() 
+        value = input()
+        if value == "1":
+             main_chart()
+        elif value == "2" :
+              calories_chart()
+        else :
+              print("nothing found")
+
